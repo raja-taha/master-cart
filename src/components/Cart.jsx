6aebb4ac3851bca854e638cd58de4299ba788651
Mr_RajaG <rajataha062@gmail.com>
2023-11-27 20:41:11 +0500

    version 1.0

---
+++ src/components/Cart.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { AiFillDelete } from "react-icons/ai";
+
+const Cart = () => {
+  const { cartItems, subTotal, shipping, tax, total } = useSelector(
+    (state) => state.cart
+  );
+  const dispatch = useDispatch();
+
+  const increment = (id) => {
+    dispatch({
+      type: "increment",
+      payload: id,
+    });
+    dispatch({ type: "calculatePrice" });
+  };
+  const decrement = (id) => {
+    dispatch({
+      type: "decrement",
+      payload: id,
+    });
+    dispatch({ type: "calculatePrice" });
+  };
+  const deleteHandler = (id) => {
+    dispatch({
+      type: "delete",
+      payload: id,
+    });
+    dispatch({ type: "calculatePrice" });
+  };
+
+  return (
+    <div className="cart">
+      <main>
+        {cartItems.length > 0 ? (
+          cartItems.map((i) => (
+            <CartItem
+              imgSrc={i.imgSrc}
+              name={i.name}
+              price={i.price}
+              qty={i.quantity}
+              id={i.id}
+              key={i.id}
+              decrement={decrement}
+              increment={increment}
+              deleteHandler={deleteHandler}
+            />
+          ))
+        ) : (
+          <h1>No Items Yet</h1>
+        )}
+      </main>
+      <aside>
+        <h2>Subtotal: ${subTotal}</h2>
+        <h2>Shipping: ${shipping}</h2>
+        <h2>Tax: ${tax}</h2>
+        <h2>Total: ${total}</h2>
+      </aside>
+    </div>
+  );
+};
+
+const CartItem = ({
+  imgSrc,
+  name,
+  price,
+  qty,
+  decrement,
+  increment,
+  deleteHandler,
+  id,
+}) => (
+  <div className="cartItem">
+    <img src={imgSrc} alt={name} />
+    <article>
+      <h3>{name}</h3>
+      <p>${price}</p>
+    </article>
+    <div>
+      <button onClick={() => decrement(id)}>-</button>
+      <p>{qty}</p>
+      <button onClick={() => increment(id)}>+</button>
+    </div>
+    <AiFillDelete onClick={() => deleteHandler(id)} />
+  </div>
+);
+
+export default Cart;
---
+++ src/components/Home.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
+
+import productList from "../Data/productList";
+
+const Home = () => {
+  const dispatch = useDispatch();
+
+  const addToCartHandler = (options) => {
+    dispatch({ type: "addToCart", payload: options });
+    dispatch({ type: "calculatePrice" });
+    toast.success("Added to Cart");
+  };
+
+  return (
+    <div className="home">
+      {productList.map((i) => (
+        <ProductCard
+          key={i.id}
+          imgSrc={i.imgSrc}
+          name={i.name}
+          price={i.price}
+          id={i.id}
+          handler={addToCartHandler}
+        />
+      ))}
+    </div>
+  );
+};
+
+const ProductCard = ({ name, id, price, handler, imgSrc }) => (
+  <div className="productCard">
+    <img src={imgSrc} alt={name} />
+    <p>{name}</p>
+    <h4>${price}</h4>
+    <button onClick={() => handler({ name, price, id, quantity: 1, imgSrc })}>
+      Add to Cart
+    </button>
+  </div>
+);
+
+export default Home;
